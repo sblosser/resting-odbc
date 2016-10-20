f360f54a701d48cc72791ad73cf13b3283464c2a
Stan Blosser <coder@stanblosser.com>
2016-10-20 13:08:36 -0400

    Added parsing logic

--- server.js
+++ server.js
@@ -2,15 +2,43 @@
 
 const fs = require('fs');
 const Hapi = require('hapi');
-let db = [];
+
+let formats = {};
+const normalizedPath = require("path").join(__dirname, "formats");
+fs.readdirSync(normalizedPath).forEach(function(file) {
+  const formatName = file.split('.')[0];
+  formats[formatName] = require("./formats/" + file);
+})
 
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+let db = [];
 for (const dsn of config.dsn) {
   db[dsn.name] = require('odbc')();
   db[dsn.name].open(dsn.conn, function(err) {
     if (err) return console.log(err);
   });
+}
+
+const customEncode = function(data, table) {
+  let encoded = {};
+  if (formats[table]) {
+    console.log('Using custom encoder: ' + table);
+    encoded[table] = formats[table].encode(data);
+    return encoded;
+  } else {
+    console.log('Usting standard encoder');
+    encoded[table] = data;
+    return encoded;
+  }
+}
+
+const customDecode = function(data, table) {
+  if (formats[table]) {
+    return formats[table].decode(data);
+  } else {
+    return JSON.parse(data);
+  }
 }
 
 const server = new Hapi.Server();
@@ -24,7 +52,17 @@ const getHandler = function(request, reply) {
   let sql = "SELECT * FROM " + table;
   let bindingParameters = [];
 
-  if (request.query === {}) {
+  if (request.params.id) {
+    // we were sent an ID to get one record
+    if (formats[table].id) {
+      sql = sql + " WHERE " + formats[table].id + " = ?";
+      bindingParameters.push(request.params.id);
+    } else {
+      return reply(JSON.stringify({error: "No ID field defined"}))
+        .type('application/json')
+        .code(500);
+    }
+  } else if (request.query === {}) {
     // no query parameters
   } else {
     sql = sql + " WHERE";
@@ -34,7 +72,6 @@ const getHandler = function(request, reply) {
     }
     sql = sql.substring(0, sql.length-3);
   }
-
   db[dsn].query(sql, bindingParameters, function(err, data) {
     let statusCode = 200;
     if (err) {
@@ -47,12 +84,19 @@ const getHandler = function(request, reply) {
       if (data.length === 0) {
         statusCode = 404;
       }
-      return reply(JSON.stringify(data))
+      const encoded = customEncode(data, table);
+      return reply(JSON.stringify(encoded))
         .type('application/json')
         .code(statusCode);
     }
   });
 };
+
+server.route({
+  method: 'GET',
+  path: '/{dsn}/{table}/{id}',
+  handler: getHandler
+})
 
 server.route({
   method: 'GET',
